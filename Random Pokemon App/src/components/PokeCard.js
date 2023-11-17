a5672f49edb8a973f199d5c2d332c984b2184353
Furkan Esen <furkanesen35@gmail.com>
2023-11-17 18:21:19 +0100

    urls added

--- Random Pokemon App/src/components/PokeCard.js
+++ Random Pokemon App/src/components/PokeCard.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 const PokeCard = () => {
  const num = Math.floor(Math.random() * 151) + 1
+ const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${num}.png`
  return (
   <div>
    <h1>Pokemon #{num}</h1>
+   <img src={url} alt="" />
   </div>
  )
 }
