97c1a87405717bf7e813f5a7df5a08f67179e9af
Furkan Esen <furkanesen35@gmail.com>
2023-11-18 15:57:45 +0100

    end of the project

--- Slot Machine/src/components/Results.js
+++ Slot Machine/src/components/Results.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
-const Results = () => {
+const Results = ({rand1,rand2,rand3}) => {
+ const isWinner = rand1 === rand2 && rand2 === rand3
  return (
-  <div>Results</div>
+  <div>
+   <h1>{rand1} {rand2} {rand3}</h1>
+   {isWinner ? "You win!" : "You lose :("}
+  </div>
  )
 }
 
--- Slot Machine/src/components/Slots.js
+++ Slot Machine/src/components/Slots.js
@@ -2,10 +2,14 @@ import React from 'react'
 import Results from './Results'
 
 const Slots = () => {
+ const arr = ["😀","☹️","🤓"]
+ const rand1 = arr[Math.floor(Math.random() * 3)]
+ const rand2 = arr[Math.floor(Math.random() * 3)]
+ const rand3 = arr[Math.floor(Math.random() * 3)]
  return (
   <div>
    <h3>Try your Luck!</h3>
-   <Results/>
+   <Results rand1={rand1} rand2={rand2} rand3={rand3}/>
    <br />
    <button>Pull the Lever</button>
   </div>
