59a6ee302962c662fc617ce8c0e925a92a7ff211
Furkan Esen <furkanesen35@gmail.com>
2023-11-18 15:40:59 +0100

    slots linked

--- Slot Machine/src/App.js
+++ Slot Machine/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Slots from './components/Slots';
 
 function App() {
  return (
   <div className="App" styles=" background-color: blue;">
    <Header/>
+   <Slots/>
   </div>
  );
 }
