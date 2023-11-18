00445b3ab932ad0aedafbcbd7f4d3fca14fc0de6
Furkan Esen <furkanesen35@gmail.com>
2023-11-18 15:39:28 +0100

    header linked

--- Slot Machine/src/App.js
+++ Slot Machine/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
  return (
   <div className="App" styles=" background-color: blue;">
-   asd
+   <Header/>
   </div>
  );
 }
