fe88f9e25f789a3ae07addc01e13467633541d0f
Furkan Esen <furkanesen35@gmail.com>
2023-11-18 15:44:57 +0100

    results linked

--- Slot Machine/src/components/Slots.js
+++ Slot Machine/src/components/Slots.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Results from './Results'
 
 const Slots = () => {
  return (
   <div>
    <h3>Try your Luck!</h3>
+   <Results/>
+   <br />
    <button>Pull the Lever</button>
   </div>
  )
