b5a59dd5ddad45c1c9c3400bff48229e0e2478b0
Furkan Esen <furkanesen35@gmail.com>
2023-11-19 13:19:05 +0100

    data done

--- Shopping List/src/App.js
+++ Shopping List/src/App.js
@@ -4,10 +4,16 @@ import List from './components/List';
 
 
 function App() {
+ const data = [ 
+  {item: "eggs", completed: false, quantity: 12},
+  {item: "milk", completed: true, quantity: 1},
+  {item: "chicken breast", completed: false, quantity: 4},
+  {item: "carrots", completed: true, quantity: 6},
+ ]
  return (
   <div className="App">
    <Header/>
-   <List/>
+   <List items={data} />
   </div>
  );
 }
