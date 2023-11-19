8cc917b52ba8ad0c490b77d320c84ed34aa9b1cb
Furkan Esen <furkanesen35@gmail.com>
2023-11-19 13:15:41 +0100

    components linked

--- Shopping List/src/App.js
+++ Shopping List/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Header from './components/Header';
+import List from './components/List';
+
 
 function App() {
-  return (
-    <div className="App">
- 
-    </div>
-  );
+ return (
+  <div className="App">
+   <Header/>
+   <List/>
+  </div>
+ );
 }
 
 export default App;
