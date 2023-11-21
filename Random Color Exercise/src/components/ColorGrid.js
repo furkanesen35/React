8699679d7a28c1065e576b09cb94a9a3e34a12c4
Furkan Esen <furkanesen35@gmail.com>
2023-11-21 22:56:23 +0100

    end of the project

--- Random Color Exercise/src/components/ColorGrid.js
+++ Random Color Exercise/src/components/ColorGrid.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import RandomColor from './RandomColor'
+import "./ColorGrid.css"
 
-const ColorGrid = () => {
+const ColorGrid = ({colors}) => {
  return (
-  <div>ColorGrid</div>
+  <div className='container'>
+   <div className='first-row'>
+    <RandomColor colors={colors}/>
+    <RandomColor colors={colors}/>
+    <RandomColor colors={colors}/>
+    <RandomColor colors={colors}/>
+   </div>
+   <div className='second-row'>
+    <RandomColor colors={colors}/>
+    <RandomColor colors={colors}/>
+    <RandomColor colors={colors}/>
+    <RandomColor colors={colors}/>
+   </div>
+   <div className='third-row'>
+    <RandomColor colors={colors}/>
+    <RandomColor colors={colors}/>
+    <RandomColor colors={colors}/>
+    <RandomColor colors={colors}/>
+   </div>
+   <div className='fourth-row'>
+    <RandomColor colors={colors}/>
+    <RandomColor colors={colors}/>
+    <RandomColor colors={colors}/>
+    <RandomColor colors={colors}/>
+   </div>
+  </div>
  )
 }
 
