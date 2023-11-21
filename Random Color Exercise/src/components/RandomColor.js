aeb7dd7af550b50906fc25589d2d9f5af9e25f94
Furkan Esen <furkanesen35@gmail.com>
2023-11-21 22:04:16 +0100

    randomness

--- Random Color Exercise/src/components/RandomColor.js
+++ Random Color Exercise/src/components/RandomColor.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./RandomColor.css"
 
 const RandomColor = ({ colors }) => {
+ const [color, setColor] = useState("purple")
+ const handleClick = () => {
+  let rndm = Math.floor(Math.random() * 16)
+  setColor(colors[rndm])
+ }
  return (
-  <div className='ColorBox' style={{ backgroundColor: "red" }}>
-
-  </div>
+  <div
+    className='ColorBox' 
+    onClick={handleClick} 
+    style={{ backgroundColor: color }}
+  ></div>
  )
 }
 
