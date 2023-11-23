39bf04a413c2b0024b881ab5d19a5bdccee16b11
Furkan Esen <furkanesen35@gmail.com>
2023-11-23 14:23:52 +0100

    adding part was done

--- Random Emoji/src/components/Emojis.js
+++ Random Emoji/src/components/Emojis.js
@@ -1,22 +1,31 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react';
+import { v4 as uuid } from 'uuid';
 
 const Emojis = () => {
- const arr = ["😀", "😃", "😄", "😁", "😆", "😅",
+  const arr = ["😀", "😃", "😄", "😁", "😆", "😅",
               "😂", "🤣", "😊", "😇", "🙂", "🙃",
               "😉", "😌", "😍", "😘", "😗", "😙",
               "😚", "😋", "😜", "😝", "😛", "🤑",
               "🤗", "🤓", "😎", "🤡", "🤠", "😏",
-              "😒", "😞", "😔", "😟", "😕", "🤢"]
- const [emoji, setEmoji] = useState([])
- const handleClick = () => {
-  setEmoji((old) => [...old, arr[Math.floor(Math.random() * 36)]]);
- }
- return (
-  <div>
-   <h3>{emoji}</h3>
-   <button onClick={() => handleClick()} >Add Random Emoji</button>
-  </div>
- )
-}
+              "😒", "😞", "😔", "😟", "😕", "🤢"];
 
-export default Emojis
+  const [emojis, setEmojis] = useState([{ id: uuid(), emoji: "😀" }]);
+
+  const handleClick = () => {
+    let rndm = arr[Math.floor(Math.random() * 36)];
+    setEmojis((old) => [...old, { id: uuid(), emoji: rndm }]);
+  };
+
+  return (
+    <div>
+      <h3>
+       {emojis.map((obj) => {
+        return <span key={obj.id}>{obj.emoji}</span>
+       })}
+      </h3>
+      <button onClick={handleClick}>Add Random Emoji</button>
+    </div>
+  );
+};
+
+export default Emojis;
