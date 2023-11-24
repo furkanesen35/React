0025a585920cfe043c14ff3fdde132a0571e462d
Furkan Esen <furkanesen35@gmail.com>
2023-11-24 15:14:07 +0100

    comp

--- Score Keeper Exercise/src/App.js
+++ Score Keeper Exercise/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Header from './components/Header';
+import Player from './components/Player';
 
 function App() {
  return (
   <div className="App">
    <Header/>
+   <Player/>
+   <Player/>
+   <Player/>
+   <Player/>
   </div>
  );
 }
---
+++ Score Keeper Exercise/src/components/Player.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Player = () => {
+ return (
+  <div className='container' >
+   <span>Player 1: 0</span>
+   <button>+1</button>
+  </div>
+ )
+}
+
+export default Player
