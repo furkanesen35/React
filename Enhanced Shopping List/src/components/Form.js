b80e1cfffaf66885c88568d9c377c9a64986494f
Furkan Esen <furkanesen35@gmail.com>
2023-11-26 17:03:41 +0100

    forms linked

---
+++ Enhanced Shopping List/src/components/Form.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>
+     <form action="">
+      <input type="text" name="" id="" />
+     </form>
+    </div>
+  )
+}
+
+export default Form
