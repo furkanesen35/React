ac371bcddc2abbe198745d96c318e55500b8a2da
Furkan Esen <furkanesen35@gmail.com>
2023-11-27 08:10:27 +0100

    header linked

--- Enhanced Shopping List/src/App.js
+++ Enhanced Shopping List/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Header from './components/Header';
-import List from './components/List';
 
 function App() {
  return (
   <div className="App">
    <Header/>
-   <List/>
   </div>
  );
 }
