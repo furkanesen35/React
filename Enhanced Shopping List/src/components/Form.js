53e5818e120855f5b85e0e3aabb14cfdbdc2833a
Furkan Esen <furkanesen35@gmail.com>
2023-11-27 07:42:07 +0100

    form done

--- Enhanced Shopping List/src/components/Form.js
+++ Enhanced Shopping List/src/components/Form.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Form = () => {
+ const [formData, setFormData] = useState({ product: "", quantity: "" })
+ const handleChange = (e) => {
+  const changedField = e.target.name
+  const newValue = e.target.value
+  setFormData(currData => {
+   currData[changedField] = newValue
+   return {...currData}
+  })
+ }
+ console.log(formData);
  return (
   <div>
-   
+   <label htmlFor="product">Product: </label>
+   <input 
+     type="text"
+     name="product"
+     id="product"
+     value={formData.product}
+     onChange={handleChange}
+   />
+   <label htmlFor="quantity">Quantity: </label>
+   <input 
+     type="number"
+     name="quantity"
+     id="quantity"
+     value={formData.quantity}
+     onChange={handleChange}
+   />
   </div>
  )
 }
