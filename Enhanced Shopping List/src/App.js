6ff26d125e54f8dc6099b9f6451f2d78a1f3f4bd
Furkan Esen <furkanesen35@gmail.com>
2023-11-27 07:07:51 +0100

    from the start

--- Enhanced Shopping List/src/App.js
+++ Enhanced Shopping List/src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-import Form from './components/Form';
-import Header from './components/Header';
 
 function App() {
  return (
   <div className="App">
-   <Header/>
-   <Form/>
   </div>
  );
 }
