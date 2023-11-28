5e44e66b6ba91ed0d6f9018af364ab76bb3f4a8a
Furkan Esen <furkanesen35@gmail.com>
2023-11-28 12:11:31 +0100

    start of the project

---
+++ react-todolist/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import CssBaseline from "@mui/material/CssBaseline"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <CssBaseline/>
+    </>
+  )
+}
+
+export default App
