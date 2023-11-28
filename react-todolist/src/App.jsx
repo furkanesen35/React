40dd6149e23fb5dbd1a661eaa80c2c35a1120d8b
Furkan Esen <furkanesen35@gmail.com>
2023-11-28 12:42:52 +0100

    list is done

--- react-todolist/src/App.jsx
+++ react-todolist/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import CssBaseline from "@mui/material/CssBaseline"
+import TodoList from './TodoList'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
      <CssBaseline/>
+     <h1>Todos</h1>
+     <TodoList/>
     </>
   )
 }
