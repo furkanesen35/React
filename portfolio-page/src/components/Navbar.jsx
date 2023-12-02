cc7b0e90866451bc22da104bc0fab94074a82a73
Furkan Esen <furkanesen35@gmail.com>
2023-12-02 23:50:31 +0100

    navbar corrected

--- portfolio-page/src/components/Navbar.jsx
+++ portfolio-page/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
  return (
-  <div className='flex justify-evenly items-center w-[900px] h-[70px] font-sans text-[25px]'>
+  <div className='flex flex-col justify-start items-center w-[900px] h-[70px] font-sans text-[25px] mobile-l:justify-evenly mobile-l:flex-row'>
    <li className='list-none m-[15px]'>Contact</li>
    <li className='list-none m-[15px]'>About</li>
    <li className='flex list-none bg-[#24a0ed] m-[15px] rounded-[15px] min-w-[180px] justify-center'>Download Cv</li>
--- portfolio-page/tailwind.config.js
+++ portfolio-page/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "mobile-l": "455px",
+      },
+    },
   },
   plugins: [],
 }
