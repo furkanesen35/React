fd1ba6056c757f1bfea00d98a0404d1bae1e5af4
Furkan Esen <furkanesen35@gmail.com>
2023-12-02 23:24:43 +0100

    navbar added

--- portfolio-page/src/App.js
+++ portfolio-page/src/App.js
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 
 function App() {
  return (
-  <div className="h-[955px] bg-black text-white">
+  <div className="flex justify-center h-[955px] bg-black text-white w-[100vw-5px]">
    <Navbar/>
   </div>
  );
--- portfolio-page/src/components/Navbar.jsx
+++ portfolio-page/src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
  return (
-  <div>
-   <li>Contact</li>
-   <li>About</li>
-   <li>Download Cv</li>
+  <div className='flex justify-evenly items-center w-[900px] h-[70px] font-sans text-[25px]'>
+   <li className='list-none m-[15px]'>Contact</li>
+   <li className='list-none m-[15px]'>About</li>
+   <li className='flex list-none bg-[#24a0ed] m-[15px] rounded-[15px] min-w-[180px] justify-center'>Download Cv</li>
   </div>
  )
 }
