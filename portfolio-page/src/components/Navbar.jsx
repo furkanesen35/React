ba91dfed19507a1b6516ee0daa3c4fd08148ab66
Furkan Esen <furkanesen35@gmail.com>
2023-12-04 12:55:35 +0100

    start of the project

--- portfolio-page/src/components/Navbar.jsx
+++ portfolio-page/src/components/Navbar.jsx
@@ -2,16 +2,12 @@ import React from 'react'
 
 const Navbar = () => {
  return (
-<<<<<<< HEAD
   <div className='flex flex-col justify-start items-center w-[900px] h-[70px] font-sans text-[25px] mobile-l:justify-evenly mobile-l:flex-row'>
    <span className='mobile-l:hidden'>Furkan Esen's Website</span>
    <li className='list-none m-[15px] cursor-pointer'>Contact</li>
    <li className='list-none m-[15px] cursor-pointer'>About</li>
    <li className='list-none m-[15px] bg-[#24a0ed] rounded-[15px] min-w-[180px] flex justify-center cursor-pointer'>Download Cv</li>
   </div>
-=======
-  <div className=''>Navbar</div>
->>>>>>> b606c6ffd80bf72da3788e7e4ccdfb56e6fc51e2
  )
 }
 
